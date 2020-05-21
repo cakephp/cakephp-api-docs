a201c7903262f93f1031d5c67fe9d109bf924d78
Corey Taylor <corey.taylor.fl@gmail.com>
2020-05-21 00:44:20 -0500

    Added DocBlock validation and placeholders.

--- src/Util/ClassLikeCollapser.php
+++ src/Util/ClassLikeCollapser.php
@@ -6,10 +6,18 @@ namespace Cake\ApiDocs\Util;
 use InvalidArgumentException;
 use phpDocumentor\Reflection\DocBlock;
 use phpDocumentor\Reflection\DocBlock\Description;
+use phpDocumentor\Reflection\DocBlock\Tags\Param;
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
+use phpDocumentor\Reflection\DocBlock\Tags\InvalidTag;
 use phpDocumentor\Reflection\Element;
 use phpDocumentor\Reflection\Php\Class_;
+use phpDocumentor\Reflection\Php\Constant;
 use phpDocumentor\Reflection\Php\Interface_;
+use phpDocumentor\Reflection\Php\Method;
+use phpDocumentor\Reflection\Php\Property;
 use phpDocumentor\Reflection\Php\Trait_;
+use phpDocumentor\Reflection\Php\Visibility;
+use phpDocumentor\Reflection\Types\Mixed_;
 
 /**
  * ClassLikeCollapser
@@ -51,7 +59,7 @@ class ClassLikeCollapser
             throw new InvalidArgumentException("Unable to find {$classlike->getFqsen()}.");
         }
 
-        $inheritance = $this->getInheritance($source);
+        $inheritance = $this->getInheritanceChain($source);
 
         return new CollapsedClassLike(
             $source,
@@ -72,7 +80,10 @@ class ClassLikeCollapser
         foreach ($inheritance as $source) {
             if (method_exists($source->getElement(), $getter)) {
                 foreach ($source->getElement()->{$getter}() as $fqsen => $element) {
-                    $elements[$element->getName()][] = $this->loader->find((string)$fqsen);
+                    $source = $this->loader->find((string)$fqsen);
+                    if ((string)$source->getElement()->getVisibility() !== 'private') {
+                        $elements[$element->getName()][] = $source;
+                    }
                 }
             }
         }
@@ -80,10 +91,77 @@ class ClassLikeCollapser
 
         foreach ($elements as &$sources) {
             $docBlock = $this->collapseDocBlock($sources);
-            $sources = ['source' => reset($sources), 'docBlock' => $docBlock];
+            $docBlock = $this->validateDocBlock($sources[0], $docBlock);
+            $sources = ['source' => $sources[0], 'docBlock' => $docBlock];
         }
 
         return $elements;
+    }
+
+    /**
+     * Validates docblock against element source and adds placeholders where needed.
+     *
+     * @param \Cake\ApiDocs\Util\LoadedFqsen $source source
+     * @param \phpDocumentor\Reflection\DocBlock $docBlock docblock
+     * @return \phpDocumentor\Reflection\DocBlock
+     */
+    protected function validateDocBlock(LoadedFqsen $source, DocBlock $docBlock): DocBlock
+    {
+        foreach ($docBlock->getTags() as $tag) {
+            if ($tag instanceof InvalidTag) {
+                if ($source->getInProject()) {
+                    api_log('warning', "Found invalid @{$tag->getName()} for `{$source->getFqsen()}`.");
+                }
+                $docBlock->removeTag($tag);
+            }
+        }
+
+        $addedTags = [];
+        if ($source->getElement() instanceof Constant || $source->getElement() instanceof Property) {
+            $tags = $docBlock->getTagsByName('var');
+            if (count($tags) === 0) {
+                if ($source->getInProject()) {
+                    api_log('warning', "Missing @var for `{$source->getFqsen()}`.");
+                }
+                $addedTags[] = new Var_($source->getElement()->getName(), new Mixed_());
+            }
+        }
+
+        if ($source->getElement() instanceof Method) {
+            $tags = $docBlock->getTagsByName('param');
+            foreach ($source->getElement()->getArguments() as $argument) {
+                $matchedTag = false;
+                foreach ($tags as $tag) {
+                    if ($tag->getVariableName() === $argument->getName()) {
+                        $matchedTag = true;
+                        break;
+                    }
+                }
+                if (!$matchedTag) {
+                    if (
+                        $source->getInProject() &&
+                        !($source->getParent() instanceof Trait_ && $this->isDocBockInheriting($docBlock))
+                    ) {
+                        api_log('warning', "Missing @param for `{$argument->getName()}` in `{$source->getFqsen()}`.");
+                    }
+                    $addedTags[] = new Param($argument->getName(), $argument->getType(), $argument->isVariadic());
+                }
+            }
+        }
+
+        if (count($addedTags) > 0) {
+            $docBlock = new DocBlock(
+                $docBlock->getSummary(),
+                $docBlock->getDescription(),
+                array_merge($addedTags, $docBlock->getTags()),
+                $docBlock->getContext(),
+                $docBlock->getLocation(),
+                $docBlock->isTemplateStart(),
+                $docBlock->isTemplateEnd()
+            );
+        }
+
+        return $docBlock;
     }
 
     /**
@@ -92,7 +170,7 @@ class ClassLikeCollapser
      * @param \Cake\ApiDocs\Util\LoadedFqsen[] $sources sources
      * @return \phpDocumentor\Reflection\DocBlock
      */
-    protected function collapseDocBlock(array $sources): ?DocBlock
+    protected function collapseDocBlock(array $sources): DocBlock
     {
         $summary = '';
         $description = '';
@@ -110,7 +188,7 @@ class ClassLikeCollapser
             $description_tags = array_merge($description_tags, $docBlock->getDescription()->getTags());
             $tags = array_merge($tags, $docBlock->getTags());
 
-            if (!preg_match('/@inheritDoc/i', $docBlock->getSummary())) {
+            if (!$this->isDocBockInheriting($docBlock)) {
                 break;
             }
         }
@@ -124,7 +202,7 @@ class ClassLikeCollapser
      * @param \Cake\ApiDocs\Util\LoadedFqsen $source loaded fqsen
      * @return \Cake\ApiDocs\Util\LoadedFqsen[]
      */
-    protected function getInheritance(LoadedFqsen $source): array
+    protected function getInheritanceChain(LoadedFqsen $source): array
     {
         $walker = function (Element $element, string $getter): array {
             if (!method_exists($element, $getter)) {
@@ -136,7 +214,7 @@ class ClassLikeCollapser
             foreach ($elements as $fqsen) {
                 $source = $this->loader->find((string)$fqsen);
                 if ($source) {
-                    $inheritance += $this->getInheritance($source);
+                    $inheritance += $this->getInheritanceChain($source);
                 }
             }
 
@@ -150,5 +228,25 @@ class ClassLikeCollapser
         }
 
         return $inheritance;
+    }
+
+    /**
+     * Checks if a docblock is inheriting.
+     *
+     * @param \phpDocumentor\Reflection\DocBlock $docblock docblock
+     * @return bool
+     */
+    protected function isDocBockInheriting(DocBlock $docBlock): bool
+    {
+        foreach ($docBlock->getTags() as $tag) {
+            if (preg_match('/inheritDoc/i', $tag->getName()) === 1) {
+                return true;
+            }
+        }
+        if (preg_match('/@inheritDoc/i', $docBlock->getSummary()) === 1) {
+            return true;
+        }
+
+        return false;
     }
 }
